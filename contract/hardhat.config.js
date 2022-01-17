b8cf34e20b035d314d7a19e15433f19291aa8143
VJS <minsung_96@naver.com>
2022-01-18 00:07:06 +0900

    Ethereum Build #4

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -1,13 +1,13 @@
 // https://eth-ropsten.alchemyapi.io/v2/LBuniSk3Sjg9tX3BIwVtxHVk1v0PeaGV
 
-require('@nomiclabs/hardhat-waffle')
+require('@nomiclabs/hardhat-waffle');
 
-module.exports = (
+module.exports = {
   solidity:'0.8.0',
   networks: {
     ropsten: {
       url:'https://eth-ropsten.alchemyapi.io/v2/LBuniSk3Sjg9tX3BIwVtxHVk1v0PeaGV',
-      accounts:['']
+      accounts:['2e06e0c77e607e007c2727c7e18412138d054a0b82d4b78fc1f86fac1832458b']
     }
   }
-)
+}
