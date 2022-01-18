3275a273799a4beb1480a2712dabccbaf6388652
VJS <minsung_96@naver.com>
2022-01-18 23:57:39 +0900

    Connection #1

---
+++ client/src/context/TransactionContext.jsx
@@ -0,0 +1,48 @@
+import react, { useEffect, useState } from react
+import { ethers } from 'ethers'
+
+import { contractABI, contractAddress} from '../utils/constants'
+
+export const TransactionContext = React.createContext()
+
+const { ethereum } = window
+
+const getEthereumContract = () => {
+    const provider = new ethers.providers.Web3Provider(ethereum)
+    const signer = provider.getSigner()
+    const transactionContract = new ethers.Contract(contractAddress, contractABI, signer)
+
+}
+
+export const TransactionProvider = ({ children }) => {
+    const [connectedAccount, setConnectedAccount] = useState('')
+
+    const checkIfWalletConnected = async () => {
+        if(!ethereum) return alert("Please Install Metamask Before your Contract.")
+        
+        const accounts = await ethereum.request({ method: 'eth_accounts'})
+    }
+
+    const connectWallet = async() => {
+        try {
+            if(!ethereum) return alert("Please Install Metamask Before your Contract.")
+            
+            const accounts = await ethereum.request({ method: 'eth_requestAccounts'})
+        
+            setCurrentAccount(accounts[0])
+        } catch (error) {
+            console.log(error)
+            throw new Error("No Ethereum Object Detected.")
+        }
+    }
+
+    useEffect(() => {
+        checkIfWalletConnected()
+    }, [])
+
+    return (
+        <TransactionContext.Provider value={{ connectWallet}}>
+            {children}
+        </TransactionContext.Provider>
+    )
+}
---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from './Transactions.json'
+
+export const contractABI = abi.abi
+export const contractAddress = '0x39895722dd24c733c83de904E55C3F108648bBc5'
